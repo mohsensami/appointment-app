19f6c2655ba01bd2d8c2ce8847b84e3fbaddb50f
mohsensami <mohsensami88@gmail.com>
2023-07-26 12:43:50 +0430

    call jsonplaceholder api and show in dom element in extension

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
-(() => {
-  alert("hi");
-})();
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((data) => {
+    document.getElementById("content").innerHTML = JSON.stringify(data);
+  });
