15013a964e3cd1de60340fb68803a0bb59793805
mohsensami <mohsensami88@gmail.com>
2023-07-26 14:13:37 +0430

    show tab menu content when click one

--- popup.js
+++ popup.js
@@ -1,5 +1,21 @@
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("content").innerHTML = JSON.stringify(data);
-  });
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     document.getElementById("content").innerHTML = JSON.stringify(data);
+//   });
+
+document.getElementById("posts").addEventListener("click", function () {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("content").innerHTML = JSON.stringify(data);
+    });
+});
+
+document.getElementById("comments").addEventListener("click", function () {
+  fetch("https://jsonplaceholder.typicode.com/comments")
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("content").innerHTML = JSON.stringify(data);
+    });
+});
