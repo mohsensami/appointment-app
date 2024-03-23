30e75cc57bbd28c4352546a5ae5fff63fd635353
mohsensami <mohsensami88@gmail.com>
2024-03-23 17:54:36 +0330

    Hero section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Hero from './_components/Hero';
 
 export default function Home() {
     return (
         <div>
-            <h2>Home</h2>
-            <Button>Button</Button>
+            <Hero />
         </div>
     );
 }
