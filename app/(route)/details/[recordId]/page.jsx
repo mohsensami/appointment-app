978b35414dcfaa693fffb85d9b9bb3287d24e8d1
mohsensami <mohsensami88@gmail.com>
2024-03-26 21:48:57 +0330

    show DoctorDetails in detail page

--- app/(route)/details/[recordId]/page.jsx
+++ app/(route)/details/[recordId]/page.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+'use client';
+import GlobalApi from '@/app/_utils/GlobalApi';
+import React, { useEffect, useState } from 'react';
+import DoctorDetail from '../_components/DoctorDetail';
+// import DoctorSuggestionList from '../_components/DoctorSuggestionList';
 
-function page() {
-    return <div>page</div>;
+function Details({ params }) {
+    const [doctor, setDoctor] = useState();
+    useEffect(() => {
+        getDoctorById();
+    }, []);
+    const getDoctorById = () => {
+        GlobalApi.getDoctorById(params.recordId).then((resp) => {
+            setDoctor(resp.data.data);
+        });
+    };
+    return (
+        <div className="p-5 md:px-10">
+            <h2 className="font-bold text-[22px]">Details</h2>
+
+            <div className="grid grid-cols-1 lg:grid-cols-4 ">
+                {/* Doctor Detail  */}
+                <div className=" col-span-3">{doctor && <DoctorDetail doctor={doctor} />}</div>
+                {/* Doctor Suggestion  */}
+                <div>{/* <DoctorSuggestionList /> */}</div>
+            </div>
+        </div>
+    );
 }
 
-export default page;
+export default Details;
